1c23774ca4f4b1f6e146aebcfbf50ec1301028fa
MarazzaM <marazza.matias@gmail.com>
2022-04-06 12:30:37 -0300

    added anim pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,29 @@ import Navbar from '../components/Navbar'
 import MetaHead from '../components/helpers/MetaHead'
 import Whatsapp from '../components/helpers/Whatsapp'
 import Footer from '../components/Footer'
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
     <MetaHead />
     <Whatsapp />
     <Navbar />
+    <motion.div
+      key={router.route}
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}
+    >
   <Component {...pageProps} />
+  </motion.div>
   <Footer />
     </>
   )
