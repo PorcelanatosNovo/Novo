8ea236321e38fd23100d705b58c79801aac198c0
MarazzaM <novoporcelanatos@gmail.com>
2022-04-08 17:13:17 -0300

    Update Contact.tsx

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -5,7 +5,7 @@ import ContacForm from '../components/ContactForm'
 
 function Contact() {
   return (
-   <div className=' w-screen	bg-[url("/marbel.webp")]  text-white'>
+   <div className=' w-screen	bg-[url("/marbel.webp")]  text-white max-w-full'>
       <div className='max-w-[90%] m-auto'> 
       <h1 className='text-5xl font-bold tracking-tight leading-tight pt-12 mb-12 text-center	 '>Contacto</h1>
       <p className='text-xl font-bold mb-4 text-center'>Visitá nuestro <a href="https://goo.gl/maps/miEBu5aWYMdr1Swd9" rel='noopener noreferrer' target='_blank' className='text-red-600 uppercase'>showroom</a> y no te pierdas las últimas novedades en nuestras redes sociales.</p>
