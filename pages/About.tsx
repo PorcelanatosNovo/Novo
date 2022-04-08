6efbe377a3a9d567b18b795f44cd5da44835a1de
MarazzaM <marazza.matias@gmail.com>
2022-04-08 01:29:48 -0300

    mmm...

--- pages/About.tsx
+++ pages/About.tsx
@@ -8,7 +8,7 @@ function About() {
     <div className='flex justify-center align-center flex-col	text-center	bg-[url("/marbel.webp")] max-w-[100vw] w-full'>
       <h1 className='lg:text-8xl text-6xl font-bold tracking-tight leading-tight mt-12 mb-6 text-white max-w-full'><span className='text-red-600'>novo</span> Porcelanatos</h1>
       <Us />
-      <NextCarousel />
+      <Buttons />
     </div>
   )
 }
