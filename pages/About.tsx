dc74f5b1448844237997b6fc9400c410ba4e629f
MarazzaM <marazza.matias@gmail.com>
2022-04-08 01:02:16 -0300

    letters too big in about

--- pages/About.tsx
+++ pages/About.tsx
@@ -6,7 +6,7 @@ import NextCarousel from '../components/NextCarousel'
 function About() {
   return (
     <div className='flex justify-center align-center flex-col	text-center	bg-[url("/marbel.webp")] '>
-      <h1 className='text-8xl font-bold tracking-tight leading-tight mt-12 mb-6 text-white'><span className='text-red-600'>novo</span> Porcelanatos</h1>
+      <h1 className='lg:text-8xl text-6xl font-bold tracking-tight leading-tight mt-12 mb-6 text-white'><span className='text-red-600'>novo</span> Porcelanatos</h1>
       <Us />
       <NextCarousel />
       <Buttons />
