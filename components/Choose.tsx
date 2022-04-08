58462c30314793b2be2e42f6ab69e62daf4dbb2a
MarazzaM <novoporcelanatos@gmail.com>
2022-04-08 15:46:53 -0300

    Mail updated

--- components/Choose.tsx
+++ components/Choose.tsx
@@ -7,7 +7,7 @@ function Choose() {
     <div className="block rounded-lg shadow-lg">
       <div className="flex flex-wrap items-center">
         <div className="grow-0 shrink-0 basis-auto block w-full lg:flex lg:w-6/12 xl:w-4/12 p-2 ">
-          <Image src="/figure.svg" alt="Trendy Pants and Shoes"
+          <Image src="/novo.webp" alt="Entrade de la tienda"
           width={400}
           height={350}
             className="w-full max-h-[100rem] rounded bg-white " />
