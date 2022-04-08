856841d7fd5de46ac20aec82719a32dd8925e091
MarazzaM <marazza.matias@gmail.com>
2022-04-08 01:21:02 -0300

    mmm

--- pages/About.tsx
+++ pages/About.tsx
@@ -5,7 +5,7 @@ import NextCarousel from '../components/NextCarousel'
 
 function About() {
   return (
-    <div className='flex justify-center align-center flex-col	text-center	bg-[url("/marbel.webp")] max-w-[100vw]'>
+    <div className='flex justify-center align-center flex-col	text-center	bg-[url("/marbel.webp")] max-w-[100vw] w-full'>
       <h1 className='lg:text-8xl text-6xl font-bold tracking-tight leading-tight mt-12 mb-6 text-white max-w-full'><span className='text-red-600'>novo</span> Porcelanatos</h1>
       <Us />
       <NextCarousel />
