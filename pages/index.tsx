95b6287da07d6cea62b486044825d16b2bcad5f6
MarazzaM <novoporcelanatos@gmail.com>
2022-04-12 22:32:18 -0300

    dynamic import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from 'next'
 import Heroe from '../components/Heroe'
-import Features from '../components/Features'
-import SocialMedia from '../components/SocialMedia'
+
 import Categories from '../components/Categories'
 import Choose from '../components/Choose'
+import dynamic from 'next/dynamic'
+
+const DynamicComponent = dynamic(() => import('../components/SocialMedia'))
+const DynamicComponent2 = dynamic(() => import('../components/Features'))
 
 const Home: NextPage = () => {
   return (
     <div className='bg-black' >
       <Heroe />
-      <Features />
+      <DynamicComponent2 />
       <Choose />
       <Categories />
-      <SocialMedia />
+      <DynamicComponent />
     </div>
   )
 }
