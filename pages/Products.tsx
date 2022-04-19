0ca56771100c870dcc8a2cb49ada209e167e3816
MarazzaM <marazza.matias@gmail.com>
2022-04-19 14:13:40 -0300

    added products

--- pages/Products.tsx
+++ pages/Products.tsx
@@ -8,6 +8,7 @@ function Products() {
         <div className='flex items-center justify-center flex-col	bg-[url("/marbel.webp")] text-center pb-4'>
       <h1 className='text-5xl font-bold tracking-tight leading-tight mt-12 mb-6 text-white'>Visita nuestra <a href="https://www.ceramicasnet.com.ar/" target='_blank' rel='noopener noreferrer' className='text-red-600 uppercase hover:text-red-700'>tienda online</a></h1>
       <p className='text-lg font-semibold mb-4 text-white'>Encontrá todos los productos en nuestro e-shop</p>
+      <p  className='text-lg font-bold mb-4 text-white uppercase'>Consultar por mensaje por precios en efectivo</p>
       <Buttons />
        <a href="https://novoporcelanatos468381923.mercadoshops.com.ar/" target='_blank' rel='noopener noreferrer'> 
         <Image 
