ba629bf36b288a7e11e638837f356f268714f013
MarazzaM <marazza.matias@gmail.com>
2022-04-19 14:41:58 -0300

    modified size button

--- components/Heroe.tsx
+++ components/Heroe.tsx
@@ -39,7 +39,7 @@ function Heroe(router:any) {
       
       </motion.div>
         </div>
-        <a href='https://walink.co/baa647' target='_blank' rel="noopener noreferrer" className='text-white bg-gradient-to-br from-purple-600 to-blue-500 duration-300 transition-all ease-in-out hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5  mr-2 mb-2 lg:min-w-[500px] lg:m-auto text-center uppercase hover:cursor-pointer font-bold '><span className='animate-pulse'>Consultar por mensaje por precios en efectivo.</span></a>
+        <a href='https://walink.co/baa647' target='_blank' rel="noopener noreferrer" className='text-white bg-gradient-to-br from-purple-600 to-blue-500 duration-300 transition-all ease-in-out hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5  mr-2 mb-2 lg:min-w-[450px] lg:m-auto text-center uppercase hover:cursor-pointer font-bold '><span className='animate-pulse'>Consultar por mensaje por precios en efectivo.</span></a>
     </div>
   )
 }
