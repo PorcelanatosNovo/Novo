36e675bd598f162f08881eb7c1d02951863a24a7
MarazzaM <marazza.matias@gmail.com>
2022-05-04 19:41:13 -0300

    added anim hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,14 @@ import Link from 'next/link'
 
 function Hero() {
   return (
-    <div className='flex flex-col justify-start lg:justify-center items-center lg:grid  lg:grid-cols-2 px-2 lg:py-12 sm:py-6 md:px-12 text-gray-800 text-center lg:text-left lg:gap-x-16 bg-[url("/marbel.webp")] bg-center min-h-screen lg:min-h-[75vh]  pt-4'>
+    <div className='flex flex-col overflow-x-hidden justify-start lg:justify-center items-center lg:grid  lg:grid-cols-2 px-2 lg:py-12 sm:py-6 md:px-12 text-gray-800 text-center lg:text-left lg:gap-x-16 bg-[url("/marbel.webp")] bg-center min-h-screen lg:min-h-[75vh]  pt-4'>
+
+<LazyMotion features={domAnimation}>
+    <m.div
+                    initial={{ opacity: 0, y: 10 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: .2 }}>
 
        <div className='mb-4'>
        <h1 className=" md:m-0  text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight lg:mb-12 text-white"><span className="text-red-600 text-7xl lg:text-9xl">novo</span> <br />Porcelanatos <br /></h1>
@@ -13,12 +20,13 @@ function Hero() {
         <a className="inline-block px-7 py-3 bg-white text-red-600 font-medium text-sm leading-snug uppercase rounded hover:text-red-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out m-2" data-mdb-ripple="true" data-mdb-ripple-color="light" href="https://novoporcelanatos468381923.mercadoshops.com.ar" target='_blank' rel='noopener noreferrer'  role="button">Tienda online</a>
         <Link href="/Comprar" passHref><span className="inline-block px-7 py-3  bg-white text-red-600 font-medium text-sm leading-snug uppercase rounded hover:text-red-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out hover:cursor-pointer" data-mdb-ripple="true" data-mdb-ripple-color="light"  >Cómo comprar</span></Link>
        </div>
-       
+       </m.div>
+</LazyMotion>
        <div>
        <LazyMotion features={domAnimation}>
     <m.div
-                    initial={{ opacity: 0, y: 10 }}
-                    whileInView={{ opacity: 1, y: 0 }}
+                    initial={{ opacity: 0, x: 10 }}
+                    whileInView={{ opacity: 1, x: 0 }}
                     viewport={{ once: true }}
                     transition={{ duration: .2 }}>
        <a href='https://novoporcelanatos468381923.mercadoshops.com.ar/' target="_blank" rel='noopener noreferrer'>
