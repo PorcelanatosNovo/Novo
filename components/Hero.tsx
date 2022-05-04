1f20634e5d023520b9af0068a7bc96588c463373
MarazzaM <marazza.matias@gmail.com>
2022-05-04 19:36:24 -0300

    corrected hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link'
 
 function Hero() {
   return (
-    <div className='flex flex-col justify-center items-center lg:grid  lg:grid-cols-2 px-2 lg:py-12 sm:py-6 md:px-12 text-gray-800 text-center lg:text-left lg:gap-x-16 bg-[url("/marbel.webp")] bg-center min-h-screen relative '>
+    <div className='flex flex-col justify-start lg:justify-center items-center lg:grid  lg:grid-cols-2 px-2 lg:py-12 sm:py-6 md:px-12 text-gray-800 text-center lg:text-left lg:gap-x-16 bg-[url("/marbel.webp")] bg-center min-h-screen lg:min-h-[75vh]  pt-4'>
 
        <div className='mb-4'>
-       <h1 className="leading-[3.3rem] mt-[-5rem] md:m-0  text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight lg:mb-12 text-white"><span className="text-red-600 text-6xl lg:text-9xl">novo</span> <br />Porcelanatos <br /></h1>
+       <h1 className=" md:m-0  text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight lg:mb-12 text-white"><span className="text-red-600 text-7xl lg:text-9xl">novo</span> <br />Porcelanatos <br /></h1>
         <a className="inline-block px-7 py-3 bg-red-600 text-white font-bold text-sm leading-snug uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="https://walink.co/baa647" target='_blank' rel='noopener noreferrer' role="button">Consultar</a>
         <a className="inline-block px-7 py-3 bg-white text-red-600 font-medium text-sm leading-snug uppercase rounded hover:text-red-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out m-2" data-mdb-ripple="true" data-mdb-ripple-color="light" href="https://novoporcelanatos468381923.mercadoshops.com.ar" target='_blank' rel='noopener noreferrer'  role="button">Tienda online</a>
         <Link href="/Comprar" passHref><span className="inline-block px-7 py-3  bg-white text-red-600 font-medium text-sm leading-snug uppercase rounded hover:text-red-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out hover:cursor-pointer" data-mdb-ripple="true" data-mdb-ripple-color="light"  >Cómo comprar</span></Link>
